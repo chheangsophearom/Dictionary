a472d4f83f7927d9a59feff0569db79b56e1261b
Sophearom Chheang <chheangsophearom7@gmail.com>
2023-08-03 17:05:56 +0700

    Second commit update spinner

--- app/src/main/java/sophearom/com/kh/dictionary/AddNewWord.kt
+++ app/src/main/java/sophearom/com/kh/dictionary/AddNewWord.kt
@@ -2,10 +2,17 @@ package sophearom.com.kh.dictionary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class AddNewWord : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_new_word)
+        val spinner_word_group = findViewById<Spinner>(R.id.spinner_word_group)
+        var listWordSpinner = arrayOf("Noun","Verb", "Adj")
+        var aa = ArrayAdapter(this,android.R.layout.simple_spinner_item,listWordSpinner)
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner_word_group.adapter =aa
     }
 }
